baed2e98709a54aa5a4ee4218fc6cab03d3d93ea
devnok <ha819ha.dev@gmail.com>
2024-11-20 13:34:19 +0900

    feat: 라우팅 추가

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import ThreeCanvas from "@/components/three/ThreeCanvas";
+
+const Home = () => {
+  return (
+    <main className="app transition-all ease-in">
+      {/* <HomeAnimateContainer /> */}
+      <ThreeCanvas />
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/routes/router.tsx
@@ -0,0 +1,21 @@
+import Home from "@/routes";
+import { Login } from "@/routes/login";
+import { Register } from "@/routes/register";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+export default router;
