996cac62461152d7609c798aafc7f1bc1ba0e6bf
devnok <ha819ha.dev@gmail.com>
2024-11-26 00:45:46 +0900

    Merge branch 'main' into seonung

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { S } from "./Style/loginstyle";
+
 export const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  return (
+    <S.Background>
+      <S.Container>
+        <S.Title>Login</S.Title>
+        <S.ContentBox>
+          <S.Input type="text" placeholder="id" />
+          <S.Input type="text" placeholder="password" />
+        </S.ContentBox>
+        <S.LoginBtn>Login</S.LoginBtn>
+        <S.AccountBox>
+          <span>Forgot password</span>
+          <span onClick={() => navigate("/register")}>Create Account</span>
+        </S.AccountBox>
+      </S.Container>
+    </S.Background>
+  );
 };
