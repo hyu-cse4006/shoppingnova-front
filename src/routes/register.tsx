fe4eb98ef24c20d67b4a696ea05a1a0ec9b69ed9
gonu <mambomul@naver.com>
2024-11-25 20:29:03 +0900

    feat: #1 로그인, 회원가입 퍼블리싱

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,3 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { S } from "./Style/registerstyle";
+
 export const Register = () => {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+  return (
+    <S.Background>
+      <S.Container>
+        <S.Title>Create Account</S.Title>
+        <S.ContentBox>
+          <div>
+            <S.Input type="text" placeholder="Email Address" />
+          </div>
+          <div>
+            <S.Input type="text" placeholder="Password" />
+            <S.Input type="text" placeholder="Confirm password" />
+          </div>
+          <div>
+            <S.Input type="text" placeholder="Username" />
+          </div>
+        </S.ContentBox>
+        <S.CreateBtn>Create Account</S.CreateBtn>
+        <S.AccountBox>
+          <span onClick={() => navigate("/login")}>Return to login</span>
+        </S.AccountBox>
+      </S.Container>
+    </S.Background>
+  );
 };
